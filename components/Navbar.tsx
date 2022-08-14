dc3db7065810ca113341bf90869dc2d3d0d4cf83
JRobsonJr <jose.araujo@vtex.com.br>
2022-08-14 14:14:15 -0300

    Improve Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,26 +3,23 @@ import { Flex, NavLink } from 'theme-ui'
 import type { FC } from 'react'
 import type { ThemeUIStyleObject } from 'theme-ui'
 
+const navbarStyle: ThemeUIStyleObject = {
+  backgroundColor: 'black',
+  height: '3rem',
+  color: 'white',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '2rem',
+  fontStyle: 'italic',
+  fontFamily: 'Bebas Neue',
+  letterSpacing: '.1rem',
+  position: 'fixed',
+  width: '100%',
+  zIndex: 1000,
+}
+
 const Navbar: FC = () => (
-  <Flex
-    as="nav"
-    sx={{
-      backgroundColor: 'black',
-      flexDirection: 'row',
-      height: '3rem',
-      color: 'white',
-      textTransform: 'lowercase',
-      alignItems: 'center',
-      justifyContent: 'center',
-      gap: '2rem',
-      fontStyle: 'italic',
-      fontFamily: 'Bebas Neue',
-      letterSpacing: '.1rem',
-      position: 'fixed',
-      width: '100%',
-      zIndex: 1000,
-    }}
-  >
+  <Flex as="nav" sx={navbarStyle}>
     <NavLink href="/">Início</NavLink>
     <NavLink href="/#newsletter">Newsletter</NavLink>
     <NavLink href="/#sobre">Sobre</NavLink>
